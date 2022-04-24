3988f496642a3891753d0cdcb1dacd555df3c917
Sk Tamim <sktamim225@gmail.com>
2022-04-24 21:34:32 +0530

    add: Added all box sahdows. Created a single component and a single json file to render all types of shadows. remove: Deleted unnecessary json files. Deleted extra shadow components

--- src/pages/shadows/boxShadows/BoxShadows.js
+++ src/pages/shadows/boxShadows/BoxShadows.js
@@ -1,18 +1,16 @@
 import { makeStyles } from "@mui/styles";
 import React from "react";
 import HeadingSecondary from "../../../components/UI/typography/HeadingSecondary";
-import DarkBoxShadows from "./DarkBoxShadows";
-import LightBoxShadows from "./LightBoxShadows";
+import BoxShadowComponent from "./BoxShadowComponent";
 
-const useStyles = makeStyles((theme) => ({
-	root: {},
+const useStyles = makeStyles({
 	mainHeading: {
 		position: "absolute",
 		left: "50%",
 		transform: "translateX(-50%)",
 		width: "max-content",
 	},
-}));
+});
 
 const BoxShadows = () => {
 	const classes = useStyles();
@@ -22,8 +20,10 @@ const BoxShadows = () => {
 				Box Shadows
 			</HeadingSecondary>
 
-			<LightBoxShadows />
-			<DarkBoxShadows />
+			<BoxShadowComponent shadowType='Light' />
+			<BoxShadowComponent shadowType='Dark' />
+			<BoxShadowComponent shadowType='Inset' />
+			<BoxShadowComponent shadowType='Colored' />
 		</>
 	);
 };
