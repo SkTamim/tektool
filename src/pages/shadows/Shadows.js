8e02439af9a5166aeaad37a6e6608dd415ff4187
Sk Tamim <sktamim225@gmail.com>
2022-04-24 21:51:46 +0530

    fix: Fixed a issue in shadows page

--- src/pages/shadows/Shadows.js
+++ src/pages/shadows/Shadows.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { Container } from "@mui/material";
 import ShadowsTab from "./shadowsTab/ShadowsTab";
-import BoxShadows from "./boxShadows/BoxShadows";
 
 const Shadows = () => {
 	return (
 		<Container maxWidth='xl' sx={{ py: 4 }} component='section'>
 			<ShadowsTab />
-			<BoxShadows />
 		</Container>
 	);
 };
