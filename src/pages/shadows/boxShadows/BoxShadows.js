b85e05aaaaee4d8dbec6e51a464a0d645189b37e
Sk Tamim <sktamim225@gmail.com>
2022-08-18 23:16:15 +0530

    separated box shadows and text shadows into different pages insted of Tabs. Updated shadow nav link in a dropdown link

--- src/pages/shadows/boxShadows/BoxShadows.js
+++ src/pages/shadows/boxShadows/BoxShadows.js
@@ -1,3 +1,4 @@
+import { Container } from "@mui/material";
 import React from "react";
 import HeadingSecondary from "../../../components/UI/typography/HeadingSecondary";
 import BoxShadowComponent from "./BoxShadowComponent";
@@ -5,7 +6,7 @@ import ShadowTypeNavigator from "./ShadowTypeNavigator";
 
 const BoxShadows = () => {
 	return (
-		<>
+		<Container maxWidth='xl' sx={{ py: 4 }} component='main'>
 			<div style={{ textAlign: "center", padding: "1rem 0" }}>
 				<HeadingSecondary>Box Shadows</HeadingSecondary>
 			</div>
@@ -15,7 +16,7 @@ const BoxShadows = () => {
 			<BoxShadowComponent shadowType='Dark' shadowId='dark-shadows' />
 			<BoxShadowComponent shadowType='Inset' shadowId='inset-shadows' />
 			<BoxShadowComponent shadowType='Colored' shadowId='colored-shadows' />
-		</>
+		</Container>
 	);
 };
 
