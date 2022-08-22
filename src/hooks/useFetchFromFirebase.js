a9b476d38e7a745a60ce603d9c6de751a2dc64f6
Sk Tamim <sktamim225@gmail.com>
2022-08-23 00:44:17 +0530

    Created a custom hook for Fetching data part by part form firebase. And implemented this hook to light box shadow component.

---
+++ src/hooks/useFetchFromFirebase.js
@@ -0,0 +1,111 @@
+import { useState, useReducer } from "react";
+import { collection, getDocs, query, startAfter } from "firebase/firestore";
+import { database } from "../firebase/FirebaseConfig";
+
+// Initial data object for reducer
+const initialDataState = {
+	loading: true,
+	error: "",
+	data: [],
+	lastData: 0,
+};
+
+// Reducer dispatch function
+const reducer = (state, action) => {
+	switch (action.type) {
+		case "FETCH_SUCCESS":
+			return {
+				loading: false,
+				data: action.data,
+				error: "",
+				lastData: action.lastData,
+			};
+		case "FETCH_ERROR":
+			return {
+				loading: false,
+				data: action.data,
+				lastData: action.lastData,
+				error: "Something went Wrong!",
+			};
+		default:
+			return state;
+	}
+};
+
+const useFetchFromFirebase = (refUrl) => {
+	const [state, dispatch] = useReducer(reducer, initialDataState);
+	const [hasMoreData, setHasMoreData] = useState(false);
+	const [nextDataLoading, setNextDataLoading] = useState(false);
+
+	const databaseReference = collection(database, refUrl);
+
+	// Function for initial data load
+	const getData = (queries) => {
+		const initialQuery = query(databaseReference, ...queries);
+		getDocs(initialQuery)
+			.then((response) => {
+				let items = [];
+				const lastDoc = response.docs[response.docs.length - 1];
+
+				response.docs.map((item) => {
+					items.push(item.data());
+				});
+				dispatch({
+					type: "FETCH_SUCCESS",
+					data: items,
+					lastData: lastDoc,
+				});
+			})
+			.catch((err) => {
+				console.log("Getting error form getData function" + err);
+				dispatch({ type: "FETCH_ERROR" });
+			});
+	};
+
+	// Function for getting next data
+	const getNextData = (queries) => {
+		const nextDataQuery = query(
+			databaseReference,
+			startAfter(state.lastData),
+			...queries
+		);
+		getDocs(nextDataQuery)
+			.then((response) => {
+				let isDocs = response.size === 0;
+				if (!isDocs) {
+					let items = [];
+					const lastDoc = response.docs[response.docs.length - 1];
+
+					response.docs.map((item) => {
+						items.push(item.data());
+					});
+					dispatch({
+						type: "FETCH_SUCCESS",
+						data: [...state.data, ...items],
+						lastData: lastDoc,
+					});
+					setNextDataLoading(false);
+				} else {
+					setHasMoreData(true);
+					setNextDataLoading(false);
+				}
+			})
+			.catch((err) => {
+				console.log("Getting error form getNextData function" + err);
+				dispatch({ type: "FETCH_ERROR" });
+			});
+	};
+
+	return {
+		data: state.data,
+		loading: state.loading,
+		error: state.error,
+		getData: getData,
+		nextDataLoading: nextDataLoading,
+		setNextDataLoading: setNextDataLoading,
+		hasMoreData: hasMoreData,
+		getNextData: getNextData,
+	};
+};
+
+export default useFetchFromFirebase;
--- src/pages/shadows/boxShadows/LightBoxShadows.js
+++ src/pages/shadows/boxShadows/LightBoxShadows.js
@@ -1,13 +1,6 @@
-import React, { useState, useEffect, useReducer } from "react";
-import { database } from "../../../firebase/FirebaseConfig";
-import {
-	collection,
-	getDocs,
-	query,
-	orderBy,
-	limit,
-	startAfter,
-} from "firebase/firestore";
+import React, { useEffect } from "react";
+import { limit } from "firebase/firestore";
+import useFetchFromFirebase from "../../../hooks/useFetchFromFirebase";
 
 import { makeStyles } from "@mui/styles";
 import { Button, Grid } from "@mui/material";
@@ -34,100 +27,27 @@ const useStyles = makeStyles((theme) => ({
 	},
 }));
 
-const initialDataState = {
-	loading: true,
-	error: "",
-	data: [],
-};
-const reducer = (state, action) => {
-	switch (action.type) {
-		case "FETCH_SUCCESS":
-			return {
-				loading: false,
-				data: action.data,
-				error: "",
-			};
-		case "FETCH_ERROR":
-			return {
-				loading: false,
-				data: action.data,
-				error: "Something went Wrong!",
-			};
-		default:
-			return state;
-	}
-};
-
 const LightBoxShadows = (props) => {
 	const classes = useStyles();
-	const [state, dispatch] = useReducer(reducer, initialDataState);
-	const [lastData, setLastData] = useState(0);
-	const [isEmpty, setIsEmpty] = useState(false);
-	const [loading, setLoading] = useState(false);
 
-	const ref = collection(database, "shadows/box-shadows/light");
-	const q = query(ref, limit(5));
-	const getData = () => {
-		getDocs(q)
-			.then((response) => {
-				let items = [];
-				response.docs.map((item) => {
-					items.push(item.data());
-				});
-				dispatch({
-					type: "FETCH_SUCCESS",
-					data: items,
-				});
-
-				const lastDoc = response.docs[response.docs.length - 1];
-				setLastData(lastDoc);
-			})
-			.catch((err) => {
-				console.log(err);
-				dispatch({ type: "FETCH_ERROR" });
-			});
-	};
+	const {
+		data,
+		loading,
+		error,
+		getData,
+		nextDataLoading,
+		setNextDataLoading,
+		hasMoreData,
+		getNextData,
+	} = useFetchFromFirebase("shadows/box-shadows/light");
 
 	useEffect(() => {
-		getData();
+		getData([limit(15)]);
 	}, []);
 
-	console.log(lastData);
-	console.log(state.data);
-
-	const getNextData = () => {
-		const newQ = query(ref, startAfter(lastData), limit(25));
-		getDocs(newQ)
-			.then((response) => {
-				let isDocs = response.size === 0;
-				if (!isDocs) {
-					let items = [];
-					response.docs.map((item) => {
-						items.push(item.data());
-					});
-					dispatch({
-						type: "FETCH_SUCCESS",
-						data: [...state.data, ...items],
-					});
-
-					const lastDoc = response.docs[response.docs.length - 1];
-					setLastData(lastDoc);
-					setLoading(false);
-				} else {
-					setIsEmpty(true);
-					setLoading(false);
-				}
-			})
-			.catch((err) => {
-				console.log(err);
-				dispatch({ type: "FETCH_ERROR" });
-			});
-	};
-
 	const loadMoreData = () => {
-		getNextData();
-		setLoading(true);
-		console.log("loding more...");
+		getNextData([limit(5)]);
+		setNextDataLoading(true);
 	};
 
 	return (
@@ -135,23 +55,23 @@ const LightBoxShadows = (props) => {
 			<h4 className={classes.subHeading}>{props.shadowType} Shadows</h4>
 
 			<Grid container gap={8} justifyContent='center' py={4}>
-				{state.loading && <LoadingScreen />}
-				{state.data &&
-					state.data.map((data) => (
+				{loading && <LoadingScreen />}
+				{data &&
+					data.map((data) => (
 						<Grid item key={data.id}>
 							<ShadowBox styles={data.shadow} by={data.by} link={data.link} />
 						</Grid>
 					))}
-				{state.error && (
-					<Paragraph className={classes.errorMessage}>{state.error}</Paragraph>
+				{error && (
+					<Paragraph className={classes.errorMessage}>{error}</Paragraph>
 				)}
 			</Grid>
 			<div style={{ textAlign: "center" }}>
-				{!loading && !isEmpty && (
+				{!nextDataLoading && !hasMoreData && (
 					<Button onClick={loadMoreData}>Load more</Button>
 				)}
-				{loading && <p>Loading more data...</p>}
-				{isEmpty && <p>No more data...</p>}
+				{nextDataLoading && <p>Loading more data...</p>}
+				{hasMoreData && <p>No more data...</p>}
 			</div>
 		</div>
 	);
