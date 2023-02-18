d3af61a0511a046f99d56f8ec78a11345b4890a5
Sk Tamim <sktamim225@gmail.com>
2023-02-18 18:47:29 +0530

    Added category in autocomplete search box in resorce page

--- src/pages/resources/Resources.js
+++ src/pages/resources/Resources.js
@@ -1,25 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Container } from "@mui/material";
 import AutoCompleteSearchBar from "../../components/searchBar/AutoCompleteSearchBar";
 import ResourceSection from "./ResourceSection";
 
-import CollectionsIcon from "@mui/icons-material/Collections";
-import WallpaperIcon from "@mui/icons-material/Wallpaper";
-import AppsIcon from "@mui/icons-material/Apps";
-
 const Resources = () => {
-	const [isSharched, setIsSharched] = useState(false);
-
-	const getSearchValue = (value) => {
-		// getSearchData(value.toLowerCase());
-		setIsSharched(true);
-	};
-
-	const getSerachLetter = (letter) => {
-		if (letter.length === 0) {
-			setIsSharched(false);
-		}
-	};
 
 	useEffect(() => {
 		document.title = "TekTool - Resources for Developers"
@@ -27,21 +11,20 @@ const Resources = () => {
 
 	return (
 		<Container maxWidth='xl' sx={{ py: 4 }} component='main'>
-			<AutoCompleteSearchBar searchList={searchList} />
+			<AutoCompleteSearchBar
+				searchList={searchList}
+			/>
 			<ResourceSection
 				fetchUrl='resources/media/images-videos'
 				heading='Free stock Images and Videos'
-				icon={<CollectionsIcon sx={{ color: "#008DA3", mr: 1 }} />}
 			/>
 			<ResourceSection
 				fetchUrl='resources/media/vector-graphics'
 				heading='Free vector graphics'
-				icon={<WallpaperIcon sx={{ color: "#008DA3", mr: 1 }} />}
 			/>
 			<ResourceSection
 				fetchUrl='resources/media/icons'
 				heading='Free Icons'
-				icon={<AppsIcon sx={{ color: "#008DA3", mr: 1 }} />}
 			/>
 		</Container>
 	);
@@ -50,7 +33,7 @@ const Resources = () => {
 export default Resources;
 
 const searchList = [
-	"Free Stock Images and Videos",
-	"Free vector graphics",
-	"Free Icons",
+	{ title: "Free Stock Images and Videos", category: 'Media' },
+	{ title: "Free vector graphics", category: 'Media' },
+	{ title: 'Free Icons', category: 'Media' }
 ];
